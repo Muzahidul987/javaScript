4a9b75c0dcb5ddf2ac2dde044c76c4260b2bb370
Muzahidul987 <muzahidul987@gmail.com>
2023-01-11 12:52:11 +0600

    basic javascript

---
+++ js1_basic_js_and_arithmetic_operation.js
@@ -0,0 +1,52 @@
+ console.log("Welcome to java script (js)");
+
+//  this is for comments 
+
+var first_number;
+let second_number;
+const third_number=30;
+fourth_number=300;
+
+console.log(first_number);
+console.log(second_number);
+console.log(third_number);
+console.log(fourth_number);
+ 
+/* conts is use for constant data and let is use for variable data.
+other two method is not useing now. */
+
+let y, a, b, c, sum_result, sub_result, mul_result, div_resutl, mod_result;
+
+const x=33;
+y=10;
+a=20;
+b=5
+c=30;
+sum_result= x + y;
+sub_result= c - b;
+mul_result= a * b;
+div_result= c / b;
+mod_result= x % b;
+
+console.log(x + y);
+console.log(x * c);
+console.log(c - x);
+console.log(c - a);
+console.log( c / a);
+console.log(x % b);
+
+console.log(sum_result);
+console.log(sub_result);
+console.log(mul_result);
+console.log(div_result);
+console.log(mod_result);
+
+console.log( "sum: "+ sum_result);
+console.log('sub:'+sub_result);
+console.log('mul:'+mul_result);
+console.log("div:"+div_result);
+console.log("mod:"+mod_result);
+
+
+
+
